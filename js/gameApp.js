8a398899eed5f5aad8d95238a62e4225cca856bb
Gintare Auks <gintareauks@gmail.com>
2020-11-07 12:23:25 +0000

    continued

--- js/gameApp.js
+++ js/gameApp.js
@@ -45,6 +45,26 @@ const phraseArray = getRandomPhraseAsArray(phrases);
 addPhraseToDisplay(phraseArray);
 
 // Checking if the letter is in the phrase
+const checkLetter = button => {
+    let phraseItems = ul.children;
+    let correct = null;
+    for (let i = 0; phraseItems.length; i++) {
+        const letter = phraseItems[i].textContent.toLowerCase();
+        if (button.textContent === letter) {
+            phraseItems[i].classList.add('show');
+            correct = true;
+        }
+    } return correct;
+}
 
+//listen for the onscreen keyboard to be clicked
+qwerty.addEventListener('click', (e) => {
+    const buttonClicked = event.target;
+    if (buttonClicked.tagName === 'BUTTON') {
+        buttonClicked.classList.add('chosen');
+        buttonClicked.disabled = 'true';
 
+        const letterFound = checkLetter(buttonClicked);
+    }
+});
 
